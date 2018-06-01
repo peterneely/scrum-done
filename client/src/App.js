876c9508c18aa1c4bc2cce1953d18dbed71801b7
Peter Neely <pgneely@gmail.com>
2018-05-31 22:02:22 -0700

    Added express and sass

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,21 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = { response: '' };
+
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({ response: res.express }))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch('/api/hello');
+    const body = await response.json();
+    if (response.status !== 200) throw Error(body.message);
+    return body;
+  };
+
   render() {
     return (
       <div className="App">
@@ -12,6 +27,9 @@ class App extends Component {
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p className="App-intro">
+          {this.state.response}
         </p>
       </div>
     );
