b5c00097405706a69f78a3f39809f15d3f79d507
peterneely <pgneely@gmail.com>
2018-06-02 12:15:58 -0700

    Heroku friendly

---
+++ server/index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const path = require('path');
+const cluster = require('cluster');
+const numCPUs = require('os').cpus().length;
+
+const PORT = process.env.PORT || 5000;
+
+// Multi-process to utilize all CPU cores
+if (cluster.isMaster) {
+  console.error(`Node cluster master ${process.pid} is running`);
+
+  // Fork workers
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', (worker, code, signal) => {
+    console.error(`Node cluster worker ${worker.process.pid} exited: code ${code}, signal ${signal}`);
+  });
+
+} else {
+  const app = express();
+
+  // Priority serve any static files
+  app.use(express.static(path.join(__dirname, 'client', 'build')));
+
+  // Answer API requests
+  app.get('/api/hello', (req, res) => {
+    res.send({ express: 'Hello From Express' });
+  });
+
+  // All remaining requests return the React app, so it can handle routing
+  app.get('/*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+  });
+
+  app.listen(PORT, () => console.log(`Node cluster worker ${process.pid}: listening on port ${PORT}`));
+}
+
